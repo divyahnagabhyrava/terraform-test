8b6178672ffc8862e2e6a0532e577d6dcd6e2add
Divya Harika Nagabhyrava <divya.nagabhyrava@noteable.io>
2025-02-18 01:19:17 -0700

    update secrets

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,7 +14,11 @@ terraform {
 
   backend "azurerm" {}
 
-  provider "azurerm" {
-    features {}
-  }
+    provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
 }
