9cd96a2c933079fccb4b1340caa4257fed17e147
Divya Harika Nagabhyrava <divya.nagabhyrava@noteable.io>
2025-02-18 07:10:41 -0700

    update provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,15 +6,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
   }
+}
 
-  backend "azurerm" {}
+provider "azurerm" {
+  features {}
 
-  provider "azurerm" {
-    features {}
-  }
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,3 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
-  default     = "example-resources"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region"
-  default     = "eastus"
-}
-
-variable "environment" {
-  type        = string
-  description = "Environment name"
-  default     = "test"
-}
-
 variable "subscription_id" {
   type        = string
   description = "Azure Subscription ID"
@@ -34,4 +16,22 @@ variable "client_id" {
 variable "client_secret" {
   type        = string
   description = "Azure Client Secret"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+  default     = "example-resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "eastus"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+  default     = "test"
 }
